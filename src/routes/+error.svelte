3120cb9b5858d209ce98b5c63c1e6e3f8bd6652b
PScoriae <pierreccesario@gmail.com>
2022-10-23 22:50:35 +0800

    feat: add custom error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+// @ts-nocheck
+
+	import '$pcss';
+  import { page } from '$app/stores';
+</script>
+
+<div class="text-4xl flex items-center justify-center h-screen">
+  {$page.status}: {$page.error.message}
+</div>
+
