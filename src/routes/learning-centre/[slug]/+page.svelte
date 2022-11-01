56ee7552a05c55a83c30514767e5e3f448fc97cb
PScoriae <pierreccesario@gmail.com>
2022-11-01 22:29:18 +0800

    style: cleanup code

--- src/routes/learning-centre/[slug]/+page.svelte
+++ src/routes/learning-centre/[slug]/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
-  import Article from "$lib/components/article.svelte";
+	import type { PageData } from './$types';
+	import Article from '$lib/components/article.svelte';
 	import '$pcss';
 
-  export let data: PageData
-  const article = data.article
+	export let data: PageData;
+	const article = data.article;
 </script>
 
 <svelte:head>
-  <title>{article.title} - Learning Centre</title>
+	<title>{article.title} - Learning Centre</title>
 </svelte:head>
 
-	<!-- <div class="mt-28 text-4xl grid justify-center">{article.title}</div> -->
-	<Article {article} />
+<Article {article} />
