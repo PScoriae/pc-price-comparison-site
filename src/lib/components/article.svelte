033415f1d88ea076f158c3407065a8ba485a673c
PScoriae <pierreccesario@gmail.com>
2022-11-01 22:28:33 +0800

    fix: article spacing

--- src/lib/components/article.svelte
+++ src/lib/components/article.svelte
@@ -3,17 +3,19 @@
 	 * @type {{ _id: string; title: string; desc: string; content: string; imgUrl: string; slug: string;}}
 	 */
 	export let article;
-	import "$pcss"
+	import '$pcss';
 </script>
 
-<div class="flex items-center justify-center h-screen">
-	<div class="card w-1/2 h-1/2 max-w-96 max-h-96 bg-base-300 shadow-lg">
-		<figure><img src={article.imgUrl} alt={article.title} /></figure>
-		<div class="card-body">
-			<h2 class="card-title">{article.title}</h2>
-			<p>{article.desc}</p>
-			<div class="card-actions justify-end">
-				<button class="btn btn-primary">Buy Now</button>
+<div class="grid h-[80vh]">
+	<div class="flex items-center justify-center">
+		<div class="card w-1/2 h-1/2 max-w-96 max-h-96 bg-base-300 shadow-lg">
+			<figure><img src={article.imgUrl} alt={article.title} /></figure>
+			<div class="card-body">
+				<h2 class="card-title">{article.title}</h2>
+				<p>{article.desc}</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary">Buy Now</button>
+				</div>
 			</div>
 		</div>
 	</div>
