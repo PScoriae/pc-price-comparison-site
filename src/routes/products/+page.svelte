8b70ddbc3033f0e31e63d3348e6b087f5c29d1b5
PScoriae <pierreccesario@gmail.com>
2022-11-01 12:27:13 +0800

    add: products page

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '$pcss';
+</script>
+
+<div class="grid h-[80vh] place-items-center">
+	<a href="/products/cpu" class="btn btn-wide">CPU</a>
+	<a href="/products/motherboard" class="btn btn-wide">Motherboard</a>
+	<a href="/products/memory" class="btn btn-wide">Memory</a>
+	<a href="/products/gpu" class="btn btn-wide">GPU</a>
+	<a href="/products/psu" class="btn btn-wide">PSU</a>
+	<a href="/products/case" class="btn btn-wide">Case</a>
+	<a href="/products/cooling" class="btn btn-wide">Cooling</a>
+</div>
