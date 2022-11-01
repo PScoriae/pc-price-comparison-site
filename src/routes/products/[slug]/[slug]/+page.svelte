e8db4b0c8f11bb7b5427c6991ad87e856427a233
PScoriae <pierreccesario@gmail.com>
2022-11-01 22:29:41 +0800

    fix: product page formatting

--- src/routes/products/[slug]/[slug]/+page.svelte
+++ src/routes/products/[slug]/[slug]/+page.svelte
@@ -8,34 +8,40 @@
 	let splitName = pcPart.name.split(' ');
 </script>
 
-<img src={pcPart.imgUrl} alt="Product" class="w-[30vh] h-[30vh] my-10" />
-<div class="stats stats-vertical shadow">
-	<div class="stat place-items-center">
-		<div class="stat-title">Product Name</div>
-		<div class="stat-value">
-			{#each splitName as name, i}
-				{#if i === 9 || i === 18 || i === 27}
-					{name} <br />
-				{:else}
-					{name}&nbsp;
-				{/if}
-			{/each}
+<div class="grid h-[80vh] place-content-center">
+	<div class="items-center">
+		<img src={pcPart.imgUrl} alt="Product" class="w-[30vh] h-[30vh] my-10 mx-auto" />
+	</div>
+	<div class="stats stats-vertical shadow">
+		<div class="stat place-items-center">
+			<div class="stat-title">Product Name</div>
+			<div class="stat-value">
+				{#each splitName as name, i}
+					{#if i === 9 || i === 18 || i === 27}
+						{name} <br />
+					{:else}
+						{name}&nbsp;
+					{/if}
+				{/each}
+			</div>
 		</div>
-	</div>
 
-	<div class="stat place-items-center">
-		<div class="stat-title">Price</div>
-		<div class="stat-value">RM{pcPart.price}</div>
-	</div>
+		<div class="stat place-items-center">
+			<div class="stat-title">Price</div>
+			<div class="stat-value">RM{pcPart.price}</div>
+		</div>
 
-	<div class="stat place-items-center">
-		<div class="stat-title">Seller</div>
-		<div class="stat-value text-secondary">{pcPart.sellerName}</div>
-	</div>
+		<div class="stat place-items-center">
+			<div class="stat-title">Seller</div>
+			<div class="stat-value text-secondary">{pcPart.sellerName}</div>
+		</div>
 
-	<div class="stat place-items-center">
-		<div class="stat-value text-secondary">
-			<a href={pcPart.itemUrl} target="_blank" rel="noreferrer" class="btn">Link to Product Page</a>
+		<div class="stat place-items-center">
+			<div class="stat-value text-secondary">
+				<a href={pcPart.itemUrl} target="_blank" rel="noreferrer" class="btn"
+					>Link to Product Page</a
+				>
+			</div>
 		</div>
 	</div>
 </div>
