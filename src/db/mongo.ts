9ce113024808c51a3b9a2bce7c671e2517a05e24
PScoriae <pierreccesario@gmail.com>
2022-11-05 13:54:18 +0800

    db: set minpoolsize and use compression

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -2,7 +2,10 @@ import { MongoClient } from 'mongodb';
 import * as dotenv from 'dotenv';
 dotenv.config();
 const url: string = process.env.MONGO_URL ?? 'mongodb://localhost:4600/pc-comparison-site';
-const client = new MongoClient(url);
+const client = new MongoClient(url, {
+	minPoolSize: 10,
+	compressors: 'zstd'
+});
 
 try {
 	await client.connect();
