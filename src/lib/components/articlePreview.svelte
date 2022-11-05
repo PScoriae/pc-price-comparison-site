56e1ae0198c6cc781a0b7f95193336ff15dd07de
PScoriae <pierreccesario@gmail.com>
2022-11-05 20:23:41 +0800

    style: use prettier formatting

--- src/lib/components/articlePreview.svelte
+++ src/lib/components/articlePreview.svelte
@@ -3,10 +3,13 @@
 	 * @type {{ _id: string; title: string; desc: string; content: string; imgUrl: string; slug: string;}}
 	 */
 	export let article;
-	import "$pcss"
+	import '$pcss';
 </script>
 
-<a href={`/learning-centre/${article.slug}`} class="grid grid-rows-3 grid-flow-col justify-evenly btn w-2/3">
+<a
+	href={`/learning-centre/${article.slug}`}
+	class="grid grid-rows-3 grid-flow-col justify-evenly btn w-2/3"
+>
 	<div class="row-span-3 ...">{article.imgUrl}</div>
 	<div class="col-span-2 ...">{article.title}</div>
 	<div class="row-span-2 col-span-2 ...">{article.desc}</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/routes/**/*.{svelte,js,ts}','./src/lib/components/**/*.{svelte,js,ts}'],
+	content: ['./src/routes/**/*.{svelte,js,ts}', './src/lib/components/**/*.{svelte,js,ts}'],
 	theme: {
 		extend: {}
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
-		themes: ["light", "dark"]
+		themes: ['light', 'dark']
 	}
 };
