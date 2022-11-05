6030d317c080294f41caa5c7ada33a916b4a679a
PScoriae <pierreccesario@gmail.com>
2022-11-06 02:54:07 +0800

    fix: change profile layout if no saved parts lists

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -58,14 +58,7 @@
 		</div>
 	</div>
 
-	{#if data.partsLists.length === 0}
-		<input
-			type="text"
-			placeholder="Search"
-			class="input input-primary input-disabled w-full max-w-xs col-start-2 row-start-2"
-			bind:value={searchTerm}
-		/>
-	{:else}
+	{#if data.partsLists.length > 0}
 		<input
 			type="text"
 			placeholder="Search"
@@ -79,57 +72,66 @@
 	</form>
 </div>
 
-<LightPaginationNav
-	totalItems={items.length}
-	{pageSize}
-	{currentPage}
-	limit={1}
-	showStepOptions={true}
-	on:setPage={(e) => (currentPage = e.detail.page)}
-/>
+{#if data.partsLists.length > 0}
+	<LightPaginationNav
+		totalItems={items.length}
+		{pageSize}
+		{currentPage}
+		limit={1}
+		showStepOptions={true}
+		on:setPage={(e) => (currentPage = e.detail.page)}
+	/>
 
-<div class="overflow-x-auto">
-	<table class="table w-full">
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Parts List ID</th>
-				<th id="view-list" />
-				<th id="delete-list" />
-			</tr>
-		</thead>
-		<tbody>
-			{#each paginatedItems as item}
-				<tr class="hover">
-					<td>
-						<div class="font-bold">{item.name}</div>
-					</td>
-					<td>{item.partsListId}</td>
-					<td><button class="btn btn-ghost" on:click={() => viewList(item)}>View List</button></td>
-					<td>
-						<button class="btn btn-ghost" on:click={() => deletePartsList(item)}>
-							Delete List
-						</button>
-					</td>
+	<div class="overflow-x-auto">
+		<table class="table w-full">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Parts List ID</th>
+					<th id="view-list" />
+					<th id="delete-list" />
 				</tr>
-			{/each}
-		</tbody>
-		<tfoot>
-			<tr>
-				<th>Name</th>
-				<th>Parts List Id</th>
-				<th id="view-list" />
-				<th id="delete-list" />
-			</tr>
-		</tfoot>
-	</table>
-</div>
+			</thead>
+			<tbody>
+				{#each paginatedItems as item}
+					<tr class="hover">
+						<td>
+							<div class="font-bold">{item.name}</div>
+						</td>
+						<td>{item.partsListId}</td>
+						<td><button class="btn btn-ghost" on:click={() => viewList(item)}>View List</button></td
+						>
+						<td>
+							<button class="btn btn-ghost" on:click={() => deletePartsList(item)}>
+								Delete List
+							</button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+			<tfoot>
+				<tr>
+					<th>Name</th>
+					<th>Parts List Id</th>
+					<th id="view-list" />
+					<th id="delete-list" />
+				</tr>
+			</tfoot>
+		</table>
+	</div>
 
-<LightPaginationNav
-	totalItems={items.length}
-	{pageSize}
-	{currentPage}
-	limit={1}
-	showStepOptions={true}
-	on:setPage={(e) => (currentPage = e.detail.page)}
-/>
+	<LightPaginationNav
+		totalItems={items.length}
+		{pageSize}
+		{currentPage}
+		limit={1}
+		showStepOptions={true}
+		on:setPage={(e) => (currentPage = e.detail.page)}
+	/>
+{:else}
+	<div class="flex justify-center my-20">
+		<a href="/configurator" class="btn btn-primary"
+			>Go to the Configurator to save parts lists and view them here!</a
+		>
+	</div>
+{/if}
