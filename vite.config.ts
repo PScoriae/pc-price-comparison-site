5ca21fdf706c436a4670d39d2de37a2042b7098c
PScoriae <pierreccesario@gmail.com>
2022-11-05 15:44:23 +0800

    feat: add version number in footer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 const config: UserConfig = {
+	define: {
+		__APP_VERSION__: JSON.stringify(pkg.version)
+	},
 	plugins: [sveltekit()]
 };
 
