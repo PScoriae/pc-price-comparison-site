56fde95033d85147710494374c0d6c114690f332
PScoriae <pierreccesario@gmail.com>
2022-11-06 22:11:09 +0800

    feat: initial change to static learning centre

--- src/routes/learning-centre/+page.svelte
+++ src/routes/learning-centre/+page.svelte
@@ -1,18 +1,43 @@
 <script lang="ts">
 	import '$pcss';
-	import ArticlePreview from '$lib/components/articlePreview.svelte';
-	import type { PageData } from './$types';
 
-	export let data: PageData;
+	const articles = [
+		{ name: 'CPU', desc: 'The brain of your PC', img: 'imglinkhere' },
+		{ name: 'Memory', desc: 'The short term memory that the CPU uses.', img: 'imglinkhere' },
+		{
+			name: 'Motherboard',
+			desc: 'A printed circuit board in which all other PC components are connected to.',
+			img: 'imglinkhere'
+		},
+		{
+			name: 'GPU',
+			desc: "An add-in card that boosts your PC's graphics compute performance.",
+			img: 'imglinkhere'
+		},
+		{ name: 'Storage', desc: 'Where you can store all your files.', img: 'imglinkhere' },
+		{ name: 'PSU', desc: 'This is what keeps your PC fed with electricity.', img: 'imglinkhere' },
+		{ name: 'Case', desc: 'Think of it as a home for all the PC components.', img: 'imglinkhere' },
+		{ name: 'Cooling', desc: "PC's get can hot so we need some cooling!", img: 'imglinkhere' }
+	];
 </script>
 
 <svelte:head>
 	<title>Learning Centre</title>
 </svelte:head>
 
-<div class="grid h-[80vh] place-items-center">
-	<div class="mt-10 text-4xl">Learning Centre</div>
-	{#each data.learningArticles as article}
-		<ArticlePreview {article} />
-	{/each}
-</div>
+{#each articles as article}
+	<div class="card lg:card-side bg-base-100 shadow-xl mt-2">
+		<figure>
+			<img src="https://placeimg.com/400/400/arch" alt={`Picture of ${article.name}`} />
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">{article.name}</h2>
+			<p>{article.desc}</p>
+			<div class="card-actions justify-end">
+				<a href={`/learning-centre/${article.name.toLowerCase()}`} class="btn btn-primary"
+					>Read more</a
+				>
+			</div>
+		</div>
+	</div>
+{/each}
---
+++ src/routes/learning-centre/psu/+page.svelte
@@ -0,0 +1,31 @@
+<img src="" alt="" srcset="" />
+
+<p>
+	The electronic equipment that carries out the instructions included in a software program is known
+	as a central processing unit (CPU), sometimes known as a central processor, main processor, or
+	simply processor. The CPU executes fundamental mathematical, logical, controlling, and
+	input/output (I/O) activities as directed by the program's instructions. In contrast, specialised
+	processors like graphics processing units and external components like main memory and I/O
+	circuitry (GPUs).
+</p>
+<div class="divider" />
+<p>
+	Although CPUs' shape, design, and implementation have evolved throughout time, their basic
+	function has remained mostly same. The arithmetic-logic unit (ALU), which performs arithmetic and
+	logic operations, processor registers, which provide operands to the ALU and store the results of
+	ALU operations, and a control unit, which coordinates the coordinated operations of the ALU,
+	registers, and other components, are the main parts of a CPU. The control unit orchestrates the
+	fetching (from memory), decoding, and execution (of instructions).
+</p>
+
+<p>
+	With one or more CPUs on a single IC chip, integrated circuit (IC) microprocessors are how most
+	current CPUs are implemented. Multi-core processors are microprocessor chips with multiple CPUs.
+	Additionally, multiple virtual or logical CPUs can be created by multitasking the separate
+	physical CPUs, or processor cores. [2]
+</p>
+
+<p>
+	These integrated devices, also known as microcontrollers or systems on a chip, can include a CPU,
+	memory, peripheral interfaces, and other computer parts (SoC).
+</p>
