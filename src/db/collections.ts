28d20f20ec102bfc8513531efe222dc864acb945
PScoriae <pierreccesario@gmail.com>
2022-11-09 22:22:26 +0800

    refactor: remove collection

--- src/db/collections.ts
+++ src/db/collections.ts
@@ -1,6 +1,5 @@
 import db from '$db/mongo';
 
-export const LearningArticles = db.collection('learning-articles');
 export const PcParts = db.collection('pc-parts');
 export const Users = db.collection('users');
 export const PartsLists = db.collection('parts-lists');
